ae04714aa8c84ca7a9611e5a8ad94789a58b7e1a
Alf Watt <alf@istumbler.net>
2025-03-06 22:36:24 -0700

    Add `attributeRuns` to `NSTextStorage` on iOS
    Have the categories refernece the native class since they are limited to various platforms

--- Sources/KitBridge/ILTextView+KitBridge.m
+++ Sources/KitBridge/ILTextView+KitBridge.m
@@ -1,8 +1,8 @@
 #import "ILTextView+KitBridge.h"
 
-@implementation ILTextView (KitBridge)
-
 #if IL_APP_KIT
+@implementation NSTextView (KitBridge)
+
 - (NSString*) text {
     return self.textStorage.string;
 }
@@ -10,13 +10,11 @@
 - (void) setText:(NSString*) text {
     [self.textStorage replaceCharactersInRange:NSMakeRange(0,self.textStorage.length) withString:text];
 }
-#endif
 
 @end
 
 // MARK: -
 
-#if IL_APP_KIT
 @implementation NSTextField (KitBridge)
 
 - (NSString*) text {
@@ -25,6 +23,34 @@
 
 - (void) setText:(NSString*) text {
     self.stringValue = text;
+}
+
+@end
+#endif
+
+// MARK: -
+
+#if IL_UI_KIT
+@implementation NSTextStorage (KitBridge)
+
+- (NSArray<NSTextStorage*>*) attributeRuns {
+    NSMutableArray* attributeRuns = NSMutableArray.new;
+
+    NSTextStorage* currentAttributeRun = NSTextStorage.new;
+    NSDictionary* currentAttributes = nil;
+    for (NSUInteger index = 0; index < self.length; index++) {
+        NSDictionary* attributes = [self attributesAtIndex:index effectiveRange:nil];
+        if (attributes != currentAttributes) {
+            if (currentAttributeRun.length > 0) {
+                [attributeRuns addObject:currentAttributeRun];
+                currentAttributeRun = NSTextStorage.new;
+            }
+            currentAttributes = attributes;
+        }
+        [currentAttributeRun appendAttributedString:[self attributedSubstringFromRange:NSMakeRange(index, 1)]];
+    }
+
+    return attributeRuns;
 }
 
 @end
--- Sources/KitBridge/include/ILTextView+KitBridge.h
+++ Sources/KitBridge/include/ILTextView+KitBridge.h
@@ -6,9 +6,31 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface ILTextView (KitBridge)
+#if IL_APP_KIT
+@interface NSTextView (KitBridge)
+
 @property(nonatomic, copy) NSString *text;
 
 @end
+
+// MARK: -
+
+@interface NSTextField (KitBridge)
+
+@property(nonatomic, copy) NSString *text;
+
+@end
+#endif
+
+// MARK: -
+
+#if IL_UI_KIT
+@interface NSTextStorage (KitBridge)
+
+/// readonly variant of attributeRuns from macOS
+@property(readonly, copy) NSArray<NSTextStorage*>* attributeRuns;
+
+@end
+#endif
 
 NS_ASSUME_NONNULL_END
